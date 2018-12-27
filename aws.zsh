e49ac482574a769db4c55b7944ee1c0152c9e2b9
eduardosasso <eduardo.sasso@gmail.com>
2018-12-27 09:45:46 -0800

    .vimrc and .zshrc

--- aws.zsh
+++ aws.zsh
@@ -1,9 +1,9 @@
 # return the name and public DNS of all running instances
-function gbotservers {   
+function gbotservers {
   ec2-describe-instances --filter instance-state-name=running | awk '/INSTANCE/{dns=$4} /TAG/{{name=$5;getline} {print "\033[1;32m" name "\033[0m" " - " dns}}'
 }
 
-function gbotservername { 
+function gbotservername {
   ec2-describe-instances -Ftag:Name=$1 | awk '/INSTANCE/{ print $13 }'
 }
 
@@ -13,17 +13,19 @@ function gbotserverip {
 }
 
 # ssh into a server using its name ex: gbot-ssh gprod_be1
-  # to see a list of all the running servers run gbot-servers
-function gbotssh {  
+# to see a list of all the running servers run gbot-servers
+function gbotssh {
   server="$(ec2-describe-instances -Ftag:Name=$1 | awk '/INSTANCE/ { print $4,$7 }')"
+  # TODO sould check the result of server and return message if not found
+
   dns=$(echo $server | awk '{print $1}')
   key=$(echo $server | awk '{print $2}')
 
-  users=('root' 'ec2-user' 'ubuntu')
+  users=('ubuntu' 'root' 'ec2-user')
 
   for user in $users; do
     ssh -i ~/.pem/$key.pem $user@$dns
-    
+
     if [ $? -eq 1 ]; then
       break
     fi
